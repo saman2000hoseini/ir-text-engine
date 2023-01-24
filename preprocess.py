1586a2ef5a09d2c5b8f0692503cca66194f57a94
Kimia Montazeri <kimia.montazeri@yahoo.com>
2023-01-24 17:26:10 +0330

    add stop word elimination

--- preprocess.py
+++ preprocess.py
@@ -11,10 +11,24 @@ with open("IR_data_news_12k.json", "r") as read_file:
     data = json.load(read_file)
 
 
+def remove_stop_words(words):
+    temp_words = words
+    j = 0
+    for i in range(0, len(temp_words)):
+        if temp_words[i] in stopwords_list():
+            words.pop(j)
+        else:
+            j += 1
+
+    return words
+
+
 def preprocess(text):
     text = normalizer.normalize(text)
-    print(text)
+    # print(text)
     words = word_tokenize(text)
+    # print(words)
+    words = remove_stop_words(words)
     print(words)
 
     for i in range(0, len(words)):
